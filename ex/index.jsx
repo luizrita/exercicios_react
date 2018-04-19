75b15d26050473a81fc7126147066593e0ba179f
Luiz Rita <luiz.ricardo@portalsoft.com.br>
2018-04-19 19:13:36 -0300

    Conclusão do contador.

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ClassComponent from './classComponent'
 
-ReactDOM.render(<ClassComponent value={10}/>, document.getElementById('app'))
+ReactDOM.render(<ClassComponent label='Contador' initialValue={10}/>, document.getElementById('app'))
