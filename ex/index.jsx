425e0cd2d0d3c3009dde6267c37fe853e8466813
Luiz Rita <luiz.ricardo@portalsoft.com.br>
2018-04-19 19:49:00 -0300

    Criatação de atualização de texto.

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
+import Field from './field'
+// import ClassComponent from './classComponent'
 
-ReactDOM.render(<ClassComponent label='Contador' initialValue={10}/>, document.getElementById('app'))
+// ReactDOM.render(<ClassComponent label='Contador' initialValue={10}/>, document.getElementById('app'))
+
+ReactDOM.render(<Field initialValue='Teste'/>, document.getElementById('app'))
