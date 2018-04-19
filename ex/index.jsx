19bb2b80566eef1b01c589e9f791c845bff682a5
Luiz Ricardo Vicente Rita <luiz.rita@amcom.com.br>
2018-04-19 17:31:56 -0300

    Iniciando contador

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ClassComponent from './classComponent'
 
-ReactDOM.render(<ClassComponent value="Componente Classe"/>, document.getElementById('app'))
+ReactDOM.render(<ClassComponent value={10}/>, document.getElementById('app'))
