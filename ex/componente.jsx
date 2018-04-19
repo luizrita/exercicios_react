cb3b874c36a9a26e58c4d91810fe30aa4bbf70e0
Luiz Ricardo Vicente Rita <luiz.rita@amcom.com.br>
2018-04-19 14:45:40 -0300

    Criando dois componentes em um unico arquivo.

--- ex/componente.jsx
+++ ex/componente.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-export default () => (
+const Primeiro = props => (
     <h1>Primeiro Componente!</h1>
 )
+
+const Segundo = props => (
+    <h1>Segundo Componente!</h1>
+)
+
+export { Primeiro, Segundo }
