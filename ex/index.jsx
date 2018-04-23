3850c767cc5877b845effab0a6dbb84cbc0d8dc0
Luiz Ricardo Vicente Rita <luiz.rita@amcom.com.br>
2018-04-23 14:15:21 -0300

    Adicionando Redux ao React.

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Field from './field'
+import fieldRedulcer from './fieldRedulcer'
 // import ClassComponent from './classComponent'
 
 // ReactDOM.render(<ClassComponent label='Contador' initialValue={10}/>, document.getElementById('app'))
 
-ReactDOM.render(<Field initialValue='Teste'/>, document.getElementById('app'))
+const reducers = combineReducers({
+    field: fieldRedulcer
+})
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)} >
+        <Field initialValue='Teste'/>
+    </Provider>
+, document.getElementById('app'))
