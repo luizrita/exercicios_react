d31942963a8e9d7e13a4e56f63b6e9a3c37e2f66
Luiz Ricardo Vicente Rita <luiz.rita@amcom.com.br>
2018-04-23 14:52:06 -0300

    Criado counter com Redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import Field from './field'
-import fieldRedulcer from './fieldRedulcer'
+// import Field from './field'
+// import fieldRedulcer from './fieldRedulcer'
 // import ClassComponent from './classComponent'
 
 // ReactDOM.render(<ClassComponent label='Contador' initialValue={10}/>, document.getElementById('app'))
 
+// const reducers = combineReducers({
+//     field: fieldRedulcer
+// })
+
+import countReducer from './countReducer'
+import Counter from './counter'
+
 const reducers = combineReducers({
-    field: fieldRedulcer
+    counter: countReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)} >
-        <Field initialValue='Teste'/>
+        <Counter/>
     </Provider>
 , document.getElementById('app'))
